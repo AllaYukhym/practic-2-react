f3eb8544e1dc8070415eb31a09c0c7e0d1dee792
AllaYukhym <yukhymenko.alla@gmail.com>
2022-12-10 15:20:17 +0100

    compled task 1

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,40 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    query: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.query);
+    this.setState({
+      query: '',
+    });
+  };
+
+  handleChangeInput = e => {
+    this.setState({
+      query: e.currentTarget.value.trim().toLowerCase(),
+    });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          onChange={this.handleChangeInput}
+          value={this.state.query}
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,10 +4,76 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+    page: 1,
+    error: null,
+    photos: [],
+    per_page: 15,
+    total_results: null,
+  };
+
+  onLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  handleFormSubmit = query => {
+    this.setState({
+      query,
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getPhotos(query, page);
+    }
+  }
+
+  getPhotos = async (query, page) => {
+    try {
+      const {
+        page: currentPage,
+        per_page,
+        photos,
+        total_results,
+      } = await ImageService.getImages(query, page);
+      this.setState(prevState => ({
+        photos: [...prevState.photos, ...photos],
+        per_page: 15,
+        total_results,
+      }));
+      console.log(photos);
+    } catch (error) {
+      this.setState({
+        error: error.message,
+      });
+    }
+  };
+
   render() {
+    const { photos, per_page, total_results, page } = this.state;
+    const pageNumber = Math.ceil(total_results / per_page);
     return (
       <>
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <SearchForm onSubmit={this.handleFormSubmit} />
+        <Grid>
+          {photos.map(({ id, avg_color, alt, src }) => (
+            <GridItem key={id}>
+              <CardItem color={avg_color}>
+                <img src={src.large} alt={alt} />
+              </CardItem>
+            </GridItem>
+          ))}
+        </Grid>
+        {photos.length > 0 && page < pageNumber && (
+          <Button onClick={this.onLoadMore}>Load more</Button>
+        )}
+        {photos.length === 0 && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
       </>
     );
   }
